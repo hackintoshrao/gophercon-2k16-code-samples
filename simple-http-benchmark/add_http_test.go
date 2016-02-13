de66d8a40d0e6e1f2e41b0ede758bc039255973e
Karthic Rao <kartronics85@yahoo.com>
2016-02-13 12:14:54 +0530

    Benchmark for http simple map and struct add

--- simple-http-benchmark/add_http_test.go
+++ simple-http-benchmark/add_http_test.go
@@ -2,4 +2,31 @@ package main
 
 import (
 	"net/http"
+	"net/http/httptest"
+	"testing"
 )
+
+func BenchmarkHandleMapAdd(b *testing.B) {
+	r := request(b, "/?first=20&second=30")
+	for i := 0; i < b.N; i++ {
+		rw := httptest.NewRecorder()
+		handleMapAdd(rw, r)
+	}
+
+}
+
+func BenchmarkHandleStructAdd(b *testing.B) {
+	r := request(b, "/?first=20&second=30")
+	for i := 0; i < b.N; i++ {
+		rw := httptest.NewRecorder()
+		handleStructAdd(rw, r)
+	}
+
+}
+func request(t testing.TB, url string) *http.Request {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return req
+}
