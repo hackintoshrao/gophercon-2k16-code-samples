65e4d2e3750f15671f94e8918b8e7dda6f2e47cd
Karthic Rao <kartronics85@yahoo.com>
2016-02-14 13:08:48 +0530

    templating added with the benchmark

--- simple-http-benchmark/add_http.go
+++ simple-http-benchmark/add_http.go
@@ -1,27 +1,47 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strconv"
 )
 
-func handleMapAdd(w http.ResponseWriter, r *http.Request) {
+type Add struct {
+	Sum int
+}
 
+func handleMapAdd(w http.ResponseWriter, r *http.Request) {
+	var html bytes.Buffer
 	first, second := r.FormValue("first"), r.FormValue("second")
 	one, _ := strconv.Atoi(first)
 	two, _ := strconv.Atoi(second)
+
 	m := map[int]int{0: one, 1: two}
+	mapSum := Add{Sum: m[0] + m[1]}
+	//parse the html file
+	t, _ := template.ParseFiles("template.html")
+	t.Execute(&html, mapSum)
+
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.Write([]byte("<h2> Sum of Maps: " + fmt.Sprint(m[0]+m[1]) + "</h2>"))
+	w.Write([]byte(html.String()))
 }
 
 func handleStructAdd(w http.ResponseWriter, r *http.Request) {
+
+	var html bytes.Buffer
 	first, second := r.FormValue("first"), r.FormValue("second")
 	one, _ := strconv.Atoi(first)
 	two, _ := strconv.Atoi(second)
+
 	m := struct{ a, b int }{one, two}
+	structSum := Add{Sum: m.a + m.b}
+
+	t, _ := template.ParseFiles("template.html")
+	t.Execute(&html, structSum)
+
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.Write([]byte("<h2> Sum of Struct: " + fmt.Sprint(m.a+m.b) + "</h2>"))
 }
